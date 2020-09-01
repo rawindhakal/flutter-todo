49c7da1e4dae5c097e592b3cacd8f4669454f31b
Rabin Dhakal <rawindhakal@gmail.com>
2020-09-01 09:58:55 +0545

    Initial Commit

---
+++ lib/screens/note_details.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../Note.dart';
+import '../database_helper.dart';
+import 'package:intl/intl.dart';
+
+class NoteDetail extends StatefulWidget {
+  final String appBarTitle;
+  final Note note;
+
+  NoteDetail(this.note, this.appBarTitle);
+  @override
+  State<StatefulWidget> createState(){
+    return NoteDetailState(this.note, this.appBarTitle);
+  }
+
+}
+
+class NoteDetailState extends State<NoteDetail>{
+  static var _priority = ['High','Low'];
+  DatabaseHelper helper = DatabaseHelper();
+  String appBarTitle;
+  Note note;
+  
+  
+  
+  NoteDetailState(this.note, this.appBarTitle);
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
---
+++ lib/screens/note_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../database_helper.dart';
+import '../Note.dart';
+import 'note_details.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Note> noteList;
+  int count = 0;
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO APP"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
+        onPressed: (){
+          navigateToDetail(Note('','',2), 'Add Note');
+        },
+      ),
+    );
+  }
+
+
+
+  void navigateToDetail(Note note, String title) async{
+    bool result = await Navigator.push(context, 
+    MaterialPageRoute(builder: (context){
+      return NoteDetail(note, title);
+    }));
+    if(result == true){
+      //todo update the view
+    }
+  }
+}
